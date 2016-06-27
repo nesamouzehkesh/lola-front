c4a3ccb3b44f59c84528d12c185bc91549089dde
nesamouzehkesh <n.mouzehkesh@gmail.com>
2016-06-27 15:02:10 +1000

    main interface

--- web/controller/routes.js
+++ web/controller/routes.js
@@ -2,10 +2,14 @@
     angular.module('mainApp')              
         .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
             $routeProvider.when("/", {
-                templateUrl: "/views/home.html"
+                templateUrl: "/startbootstrap-landing-page/home.html"
             });
             $routeProvider.when("/products", {
                 templateUrl: "/views/products.html"
+            });
+            
+            $routeProvider.when("/contact", {
+                templateUrl: "/startbootstrap-landing-page/home.html#contact"
             });
             $routeProvider.otherwise({ redirectTo: '/' });
         }]);
